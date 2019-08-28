50388aea5766cf71f54899c890908a3f18ba9d33
taylorwoodman <taylor.woodman711@gmail.com>
2019-08-27 18:40:43 -0600

    Finished MVP

--- notes.js
+++ notes.js
@@ -1,68 +1,78 @@
-async function getNotes(req, res){
+async function getNotes(req, res) {
   try {
-    const db = req.app.get('db')
+    const db = req.app.get("db");
 
-    const notes = await db.query(`SELECT * 
-    FROM notes 
+    const notes = await db.query(
+      `SELECT * FROM notes 
     WHERE user_id=${req.session.user.id}
-    ORDER BY id`)
-    
-    res.send(notes)
+    ORDER BY id`
+    );
+
+    res.send(notes);
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error)
+    console.error(error);
+    res.status(500).send(error);
   }
 }
 
-async function updateNote(req, res){
+async function updateNote(req, res) {
   try {
-    const db = req.app.get('db')
+    const db = req.app.get("db");
 
-    await db.query(`UPDATE notes 
+    await db.query(
+      `UPDATE notes 
     SET note='${req.body.note}'
-    WHERE id=${req.params.id}`)
+    WHERE id=${req.params.id}`
+    );
 
-    const notes = await db.query(`SELECT * FROM notes WHERE user_id=${req.session.user.id}
-    ORDER BY id`)
+    const notes = await db.query(
+      `SELECT * FROM notes 
+    WHERE user_id=${req.session.user.id}
+    ORDER BY id`
+    );
 
-
-    res.send(notes)
+    res.send(notes);
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error)
+    console.error(error);
+    res.status(500).send(error);
   }
 }
 
-async function addNotes(req, res){
+async function addNotes(req, res) {
   try {
-    const db = req.app.get('db')
+    const db = req.app.get("db");
 
-    await db.query(`INSERT INTO notes (user_id)
-    VALUES (${req.body.user_id})`)
+    await db.query(
+      `INSERT INTO notes (user_id)
+    VALUES (${req.body.user_id})`);
 
-    const notes = await db.query(`SELECT * FROM notes WHERE user_id=${req.session.user.id}
-    ORDER BY id`)
+    const notes = await db.query(
+      `SELECT * FROM notes
+    WHERE user_id=${req.session.user.id}
+    ORDER BY id`);
 
-    res.send(notes)
+    res.send(notes);
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error)
+    console.error(error);
+    res.status(500).send(error);
   }
 }
 
-async function deleteNotes(req, res){
+async function deleteNotes(req, res) {
   try {
-    const db = req.app.get('db')
+    const db = req.app.get("db");
 
-    await db.query(`DELETE FROM notes WHERE id=${req.params.id}`)
+    await db.query(`DELETE FROM notes WHERE id=${req.params.id}`);
 
-    const notes = await db.query(`SELECT * FROM notes WHERE user_id=${req.session.user.id}
-    ORDER BY id`)
+    const notes = await db.query(`SELECT * FROM notes WHERE user_id=${
+      req.session.user.id
+    }
+    ORDER BY id`);
 
-    res.send(notes)
+    res.send(notes);
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error)
+    console.error(error);
+    res.status(500).send(error);
   }
 }
 
@@ -71,4 +81,4 @@ module.exports = {
   updateNote,
   addNotes,
   deleteNotes
-}
+};
